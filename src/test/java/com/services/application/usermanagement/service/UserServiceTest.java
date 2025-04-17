928656247b83c716e5cbd12f83825881dff0785e
Mario Sergio Dioses Fernandez <Marzo2023$$>
2025-04-17 17:47:40 -0500

    update code

--- src/test/java/com/services/application/usermanagement/service/UserServiceTest.java
+++ src/test/java/com/services/application/usermanagement/service/UserServiceTest.java
@@ -4,11 +4,16 @@ import static org.mockito.Mockito.*;
 
 import com.services.application.usermanagement.exception.CustomException;
 import com.services.application.usermanagement.mapper.UserMapper;
+import com.services.application.usermanagement.model.dto.PhoneDto;
+import com.services.application.usermanagement.model.dto.UserDto;
+import com.services.application.usermanagement.model.entity.Phone;
 import com.services.application.usermanagement.model.entity.User;
 import com.services.application.usermanagement.repository.PhoneRepository;
 import com.services.application.usermanagement.repository.UserRepository;
 import com.services.application.usermanagement.service.impl.UserServiceImpl;
 import com.services.application.usermanagement.util.Constants;
+import com.services.application.usermanagement.util.PasswordUtil;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -29,12 +34,21 @@ class UserServiceTest {
   @Mock
   private PhoneRepository phoneRepository;
 
+  @Mock
+  private PasswordUtil passwordUtil;
+
   @Spy
   private UserMapper userMapper =
             Mappers.getMapper(UserMapper.class);
 
   @InjectMocks
   private UserServiceImpl userService;
+
+  @BeforeEach
+  void setUp() {
+    String passwordRegex = "^.{6,}$";
+    userService = new UserServiceImpl(userRepository, phoneRepository, passwordUtil, userMapper, passwordRegex);
+  }
 
   @Test
   @DisplayName("return successful when get all users")
@@ -106,12 +120,107 @@ class UserServiceTest {
             .assertNotComplete();
   }
 
+  @Test
+  @DisplayName("return error password criteria when create user")
+  void returnErrorPasswordCriteriaWhenCreateUser() {
+    UserDto userdto = createSampleUserdto();
+    userdto.setPassword("123");
+
+    var testObserver = userService.createUser(userdto).test();
+
+    testObserver.awaitTerminalEvent();
+    testObserver.assertError(throwable -> throwable instanceof CustomException &&
+            Constants.Message.ERROR_PASSWORD_CRITERIA.equals(throwable.getMessage()))
+            .assertNotComplete();
+  }
+
+  @Test
+  @DisplayName("return error email already exists when create user")
+  void returnErrorEmailAlreadyExistsWhenCreateUser() {
+    UserDto userdto = createSampleUserdto();
+    userdto.setPassword("Impka1234$");
+
+    when(userRepository.findByEmailAndIsActiveTrue(any())).thenReturn(createSampleUser());
+
+    var testObserver = userService.createUser(userdto).test();
+
+    testObserver.awaitTerminalEvent();
+    testObserver.assertError(throwable -> throwable instanceof CustomException &&
+            Constants.Message.ERROR_EMAIL_ALREADY_EXISTS.equals(throwable.getMessage()))
+            .assertNotComplete();
+  }
+
+  @Test
+  @DisplayName("return successful when create user")
+  void returnSuccessfulWhenCreateUser() {
+    UserDto userdto = createSampleUserdto();
+    userdto.setPassword("Impka1234$");
+
+    when(userRepository.findByEmailAndIsActiveTrue(any())).thenReturn(null);
+
+    when(passwordUtil.encryptPassword(any())).thenReturn("$2a$10$AkPVjkiTcd/ZQx9avzVL7aBtq");
+
+    when(userRepository.save(any())).thenReturn(createSampleUser());
+
+    var testObserver = userService.createUser(userdto).test();
+
+    testObserver.awaitTerminalEvent();
+    testObserver.assertNoErrors()
+            .assertComplete();
+  }
+
+  @Test
+  @DisplayName("return successful when update user")
+  void returnSuccessfulWhenUpdateUser() {
+    UserDto userdto = createSampleUserdto();
+    userdto.setPassword("Impka1234$");
+
+    when(userRepository.findById(any())).thenReturn(Optional.of(createSampleUser()));
+
+    when(userRepository.findByEmailAndIdNotAndIsActiveTrue(any(), any())).thenReturn(null);
+
+    when(passwordUtil.encryptPassword(any())).thenReturn("$2a$10$AkPVjkiTcd/ZQx9avzVL7aBtq");
+
+    when(userRepository.save(any())).thenReturn(createSampleUser());
+
+    var testObserver = userService.updateUser(userdto).test();
+
+    testObserver.awaitTerminalEvent();
+    testObserver.assertNoErrors()
+            .assertComplete();
+  }
+
+  private UserDto createSampleUserdto() {
+    UserDto userDto = new UserDto();
+    userDto.setName("John Doe");
+    userDto.setEmail("johndoe@example.com");
+    userDto.setPassword("ValidPassword123");
+    userDto.setPhones(List.of(createSamplePhonedto()));
+    return userDto;
+  }
+
+  private PhoneDto createSamplePhonedto() {
+    PhoneDto phoneDto = new PhoneDto();
+    phoneDto.setId(1L);
+    phoneDto.setNumber("123456789");
+    return phoneDto;
+  }
+
   private User createSampleUser() {
     User user = new User();
     user.setId("f9eeefe6-4c44-4f21-8f9f-118d8be37762");
     user.setName("John Doe");
     user.setEmail("johndoe@example.com");
     user.setActive(true);
+    user.setPhones(List.of(createSamplePhone()));
     return user;
+  }
+
+  private Phone createSamplePhone() {
+    Phone phone = new Phone();
+    phone.setId(1L);
+    phone.setNumber("123456789");
+    phone.setUser(new User());
+    return phone;
   }
 }
